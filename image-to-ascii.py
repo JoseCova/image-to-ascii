94b8b585bbb301ad855e8a534319d2251bac5d09
JoseCova <alu0101023663@ull.edu.es>
2022-03-27 20:34:00 +0100

    Now a image is displayed, also the image can be scaled

--- image-to-ascii.py
+++ image-to-ascii.py
@@ -22,20 +22,36 @@ def setup_pygame() -> Tuple[Any, Any]:
     return screen, clock
 
 
+def get_scaled_image() -> Any:
+    """Scale the loaded image."""
+
+    image = pygame.image.load("image/amogus.jpg")
+    img_width, img_height = image.get_width() * 0.7, image.get_height() * 0.7
+
+    return pygame.transform.scale(image,(int(img_width), int(img_height)))
+
+
+
 def main_game() -> None:
     """Execute the program logic."""
 
     screen, clock = setup_pygame()
     fps = 60
 
+    IMAGE = get_scaled_image()
+
+
     screen.fill((0, 0, 0))
+    screen.blit(IMAGE, (0,0))
+
     pygame.display.update()
     clock.tick(fps)
 
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                quit()
+                pygame.quit()
+                exit()
 
 
 main_game()
